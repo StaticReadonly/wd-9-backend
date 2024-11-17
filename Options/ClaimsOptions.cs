ec91ff1d52b89dd57a64d1882fb3233963639acd
StaticReadonly <klymukvlad4002@gmail.com>
2024-11-17 14:44:26 +0200

    Basic config added

---
+++ Options/ClaimsOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Options
+{
+    public class ClaimsOptions
+    {
+        public string ID { get; set; }
+        public string Role { get; set; }
+        public string[] RolesAllowedAdmin { get; set; }
+        public string[] RolesAllowedUser { get; set; }
+        public string Picture { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using WebApplication1.Options;
+
 namespace WebApplication1
 {
     public class Program
@@ -5,13 +7,64 @@ namespace WebApplication1
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var config = builder.Configuration;
+
+            config.AddJsonFile("Configs/ClaimsOptions.json");
+
             var services = builder.Services;
 
             services.AddControllers();
 
+            services.AddAuthentication()
+                .AddCookie(cfg =>
+                {
+                    var cookie = cfg.Cookie;
+                    cookie.HttpOnly = true;
+                    cookie.Name = "auth";
+                    cookie.SameSite = SameSiteMode.Strict;
+                    cookie.MaxAge = TimeSpan.FromDays(14);
+                });
+
+            var claimsOptions = config.GetSection("ClaimsOptions");
+            services.Configure<ClaimsOptions>(claimsOptions);
+            services.AddAuthorization(cfg =>
+            {
+                cfg.AddPolicy("AdminOnly", cfg =>
+                {
+                    cfg.RequireAuthenticatedUser()
+                    .RequireClaim(claimsOptions["Role"], claimsOptions["RolesAllowedAdmin"])
+                    .AddAuthenticationSchemes("Cookie");
+                });
+                cfg.AddPolicy("UserAuthorized", cfg =>
+                {
+                    cfg.RequireAuthenticatedUser()
+                    .RequireClaim(claimsOptions["Role"], claimsOptions["RolesAllowedUser"])
+                    .AddAuthenticationSchemes("Cookie");
+                });
+            });
+
+            services.AddCors(cfg =>
+            {
+                cfg.AddPolicy("DevFrontend", cfg =>
+                {
+                    cfg.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                        .WithOrigins("http://localhost:3000");
+                });
+            });
+
             var app = builder.Build();
 
             app.UseHttpsRedirection();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseCors("DevFrontend");
+            }
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.MapControllers();
 
